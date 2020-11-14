a17f8c0a9bd6416a6aa47907fdfed524c4b5af5b
Jared Chapman <jared.chapman@tcnbroadcasting.com>
2020-11-13 19:35:27 -0700

    initial test

--- api/date_test.go
+++ api/date_test.go
@@ -3,12 +3,12 @@ package api_test
 import (
 	"testing"
 
-	handler "github.com/linuxfreak003/ballistic-calculator-web/api"
+	"github.com/linuxfreak003/ballistic-calculator-web/api"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDate(t *testing.T) {
 	t.Run("TestMe Func", func(t *testing.T) {
-		assert.Equal(t, 4, handler.TestMe(2))
+		assert.Equal(t, 4, api.TestMe(2))
 	})
 }
--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
-module api
+module github.com/linuxfreak003/ballistic-calculator-web/api
 
 go 1.12
+
+require github.com/stretchr/testify v1.6.1
